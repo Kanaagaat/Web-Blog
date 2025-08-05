a46d5a46d4a45e3869390784245ad78bfca5c06e
Kanagat <sauranbaykanagat@gmail.com>
2025-08-06 02:00:34 +0500

    saving blogs

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 import express from "express";
-import bodyParser from "body-parser";
+import fs from "fs"
 
+
+
+function fileCreate(name, dataName ,data){
+
+    const article ={
+        author: name, 
+        caption: dataName,
+        textInfo: data
+    };
+
+    const jsonData = JSON.stringify(article, null, 2); // The 'null, 2' arguments add pretty-printing (indentation)
+
+    fs.writeFile(`blogPost/${name}.json`, jsonData, 'utf8', (err) => {
+        if(err){
+            throw err;
+            return;
+        }
+
+        console.log("File written succesfully");
+    })
+
+}
+
+function postCreate(){
+    const testFolder = 'blogPost';
+
+    const posts =   fs.readdir(testFolder);
+    console.log(posts);
+
+
+
+
+
+
+};
 const app = express();
 const port = 3000;
 
@@ -17,8 +52,39 @@ app.get(('/posts'), (req, res) => {
 
 
 app.post("/posts", (req, res) => {
-    console.log(req.body)
+    console.log(req.body);
+    var authorName = req.body.name;
+    var authorBlogName = req.body.blogName;
+    var authorText = req.body.text;
+
+    fileCreate(authorName, authorBlogName, authorText);
+    postCreate();
+    res.render("myBlog.ejs", {
+        name: req.body.name,
+        blogName: req.body.blogName,
+        text:  req.body.text,
+
+    });
 });
+
+app.get("/myBlog", (req, res) => {
+
+    var data =  postCreate();
+    // fs.readFile(`blogPost/${blogFiles[1]}`, function(err, data) { 
+    //     if (err) throw err; 
+
+    //     const books = JSON.parse(data); 
+    //     console.log(books); 
+    // }); 
+    
+
+    res.render("myBlog.ejs", {
+        name: authorName,
+        blogName: authorBlogName,
+        text: authorText,
+
+    });
+})
 
 app.listen(port, ()=> {
     console.log(`Server is runing on port ${port}`);
